2b0e12d4d65aa07db5ee064aa597fc07da15d5fa
timestentechnologies <timestentechnologies@gmail.com>
2026-04-25 09:51:32 +0300

    products page

--- dashboard/sidebar.php
+++ dashboard/sidebar.php
@@ -29,8 +29,9 @@ $ufile = $r[0];
         <img src="uploads/logo/<?php print $ufile?>" alt="" height="30">
       </span>
     </a>
-    <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover" id="vertical-hover" onclick="toggleSidebar()">
-      <i class="ri-record-circle-line"></i>
+    <!-- Mobile hamburger button -->
+    <button type="button" class="btn btn-sm p-2 fs-20 header-item" onclick="toggleMobileSidebar()" style="display: none;">
+      <i class="ri-menu-line"></i>
     </button>
   </div>
 
