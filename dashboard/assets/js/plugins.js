aa319c2af05666c2067b842311549d2bdac80a93
timestentechnologies <timestentechnologies@gmail.com>
2026-04-25 10:31:54 +0300

    products page

--- dashboard/assets/js/plugins.js
+++ dashboard/assets/js/plugins.js
@@ -1,1 +1,26 @@
-(document.querySelectorAll("[toast-list]")||document.querySelectorAll("[data-choices]")||document.querySelectorAll("[data-provider]"))&&(document.writeln("<script type='text/javascript' src='https://cdn.jsdelivr.net/npm/toastify-js'><\/script>"),document.writeln("<script type='text/javascript' src='assets/libs/choices.js/public/assets/scripts/choices.min.js'><\/script>"),document.writeln("<script type='text/javascript' src='assets/libs/flatpickr/flatpickr.min.js'><\/script>"));
+(function() {
+    var needsToastify = document.querySelectorAll("[toast-list]").length > 0;
+    var needsChoices = document.querySelectorAll("[data-choices]").length > 0;
+    var needsFlatpickr = document.querySelectorAll("[data-provider]").length > 0;
+    
+    if (needsToastify || needsChoices || needsFlatpickr) {
+        var scripts = [];
+        if (needsToastify) {
+            scripts.push({ src: 'https://cdn.jsdelivr.net/npm/toastify-js', async: true });
+        }
+        if (needsChoices) {
+            scripts.push({ src: 'https://cdn.jsdelivr.net/npm/choices.js@10.2.0/public/assets/scripts/choices.min.js', async: true });
+        }
+        if (needsFlatpickr) {
+            scripts.push({ src: 'https://cdn.jsdelivr.net/npm/flatpickr@4.6.13/dist/flatpickr.min.js', async: true });
+        }
+        
+        scripts.forEach(function(s) {
+            var script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src = s.src;
+            if (s.async) script.async = true;
+            document.head.appendChild(script);
+        });
+    }
+})();
