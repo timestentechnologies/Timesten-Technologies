46e070212e1094fe055344484b59b8646d21d991
timestentechnologies <timestentechnologies@gmail.com>
2026-04-26 23:22:51 +0300

    header and sidebar darkblue

--- index.php
+++ index.php
@@ -300,7 +300,7 @@ function highlight_words_js($text, $highlight_words) {
 
 
                 <?php
-				   $q="SELECT * FROM  why_us ORDER BY id DESC LIMIT 4";
+				   $q="SELECT * FROM  why_us ORDER BY id DESC LIMIT 3";
 
 
  $r123 = mysqli_query($con,$q);
@@ -325,9 +325,9 @@ while($ro = mysqli_fetch_array($r123))
   }
 
 print "
-<div class='col-12 col-md-6 col-lg-3 res-margin'>
+<div class='col-12 col-md-6 col-lg-4 res-margin'>
 <!-- Single Promo -->
-<div class='single-promo color-1 bg-hover hover-bottom text-center p-5 h-100'>
+<div class='single-promo color-1 bg-hover hover-bottom text-center p-5'>
     $icon_html
     <h3 class='mb-3'>$title</h3>
     <p>$detail</p>
